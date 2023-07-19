a47a2ff5b5fc85aaab8066b9f973d1c27985ba8e
Kent James Sumayang <kentjames2002@gmail.com>
2023-07-19 14:19:32 +0800

    added User Login/Logout and Register functions.

--- admin/routes/api.php
+++ admin/routes/api.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\UsersInfoController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * CRUD Routes
+ */
 
 // READ
 Route::get('usersinfo', [UsersInfoController::class, 'index']);
@@ -26,3 +31,15 @@ Route::put('usersinfo/{id}/edit', [UsersInfoController::class, 'update']);
 
 // DELETE
 Route::delete('usersinfo/{id}/delete', [UsersInfoController::class, 'destroy']);
+
+/**
+ * Login and Register Routes
+ */
+//REGISTER USER
+Route::post('/auth/register', [UserController::class,'create']);
+
+//LOGIN USER
+Route::post('/auth/login', [UserController::class,'login']);
+
+//LOGOUT USER
+Route::post('/auth/logout', [UserController::class,'logout']);
